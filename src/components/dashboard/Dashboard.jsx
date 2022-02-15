dc411a2644ca0751939b14f927f785922518060f
ukijo <nes-you-ukijo@hotmail.fr>
2022-02-15 19:40:08 +0100

    add dashboard unfinished  component

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Dashboard.scss';
+
+const Dashboard = () => {
+  return (
+    <div className='dashboard-container'>
+        <div className='dashboard-item item-1'>visit</div>
+        <div className='dashboard-item item-2'>tests</div>
+        <div className='dashboard-item item-3'>income</div>
+        <div className='dashboard-item item-4'>email</div>
+        <div className='dashboard-item item-5'>patients</div>
+        <div className='dashboard-item item-6'>web login</div>
+    </div>
+  )
+}
+
+export default Dashboard;
